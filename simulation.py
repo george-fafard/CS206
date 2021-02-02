62e7fa7e753046eb5529ab1a1584334e1fe00f33
George <georgeC.fafard@gmail.com>
2021-02-02 12:48:39 -0500

    loop test

--- simulation.py
+++ simulation.py
@@ -1,5 +1,18 @@
 import pybullet as p
+import time as t
+
+
+LOOPS = 1000
+current_loop = 0
 
 physicsClient = p.connect(p.GUI)
+while current_loop < LOOPS:
+
+    p.stepSimulation()
+    t.sleep(0.00166)
+    current_loop += 1
+    print(current_loop)
+
+
 
 p.disconnect()
