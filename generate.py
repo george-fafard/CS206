9c9102163ea52b0759f07e468b5dd3ad4dc1dda9
George <georgeC.fafard@gmail.com>
2021-03-29 20:27:50 -0400

    random search

--- generate.py
+++ generate.py
@@ -1,4 +1,5 @@
 import pyrosim.pyrosim as pyrosim
+import random
 
 
 # this is the data pyrosim will generate
@@ -34,10 +35,11 @@ def Generate_Brain():
     pyrosim.Send_Sensor_Neuron(name=2, linkName="FrontLeg")
     pyrosim.Send_Motor_Neuron(name=3, jointName="Torso_BackLeg")
     pyrosim.Send_Motor_Neuron(name=4, jointName="Torso_FrontLeg")
-    pyrosim.Send_Synapse(sourceNeuronName=0, targetNeuronName=3, weight=0.0)
-    pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=3, weight=0.5)
-    pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=4, weight=1.0)
-    pyrosim.Send_Synapse(sourceNeuronName=0, targetNeuronName=4, weight=0.25)
+    for i in range(0, 3):
+        print('sensor step')
+        for j in range(3, 5):
+            print('motor step')
+            pyrosim.Send_Synapse(sourceNeuronName=i, targetNeuronName=j, weight=random.uniform(-1,1))
     pyrosim.End()
 
 
